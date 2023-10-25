2c1f8d0785829e28411357d0004698902db53892
Not0nline <adrial2_nathanael@yahoo.com>
2023-10-26 01:53:33 +0700

    secara keseluruhan udah, css belom, belom selesai validasi semuanya

---
+++ account/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from book.models import Book
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    username = models.CharField(max_length = 60)
+    nama = models.CharField(max_length = 255 , default = "unknown")
+    email = models.EmailField()
+    buku_dibeli = models.ManyToManyField(Book)
+    buku_ongoing = models.ManyToManyField(Book)
+    saldo = models.IntegerField()
+    alamat = models.CharField(max_length = 1000)
+
+class Review(models.Model):
+    member = models.ForeignKey(Account, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    review_text = models.TextField()
+
+class Admin(Account):
+    orders_completed = models.IntegerField()
+    books_added = models.IntegerField()
+
+
+
+
