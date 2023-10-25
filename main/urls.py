14a45c9eb77dc3fa66af62b36a893dd2d8b086b0
DaWanAnOnli <joel.rompis@gmail.com>
2023-10-25 14:01:20 +0700

    added initial database

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('get-book/', get_book_json, name='get_book_json'),
-    path('search-book/<mode>', search_book_json, name = 'search_book_json')
+    path('get-book/<sort_mode>', get_book_json, name='get_book_json'),
+    path('search-book/<search_mode>/<sort_mode>', search_book_json, name ='search_book_json')
 ]
