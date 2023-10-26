6ead005b878bd99b78cd70c847e0b78d0473fa0b
shfiradifa <firaradifa@gmail.com>
2023-10-26 21:31:41 +0700

    sementara

--- book_info/models.py
+++ book_info/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from book.models import Book
 # from account.models import Account, Review
+from account.models import Account, Review
 import random
 
 # Create your models here.
 class Cart(models.Model):
     # member = models.ForeignKey(Account, on_delete=models.CASCADE)
+    member = models.ForeignKey(Account, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     jumlah_pembelian = models.IntegerField()
