3979207841f94d40172b41c3adb7e3fcc34c597f
shfiradifa <firaradifa@gmail.com>
2023-10-29 00:53:21 +0700

    ajax

--- book_info/views.py
+++ book_info/views.py
@@ -1,9 +1,10 @@
 from django import template
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 from book.models import Book
 from account.models import Account, Review
 from book_info.forms import BookForm
@@ -14,7 +15,7 @@ from main.models import Admin
 def show_info(request, id):
     book = get_object_or_404(Book, pk=id)
     reviews = Review.objects.filter(book=book)
-    # carts = Cart.objects.filter(member=request.user)
+    cart = Cart.objects.filter(member=request.user.account, book=book)
     context = {
         # dummy
         # 'authors': 'Leila S. Chudori',
@@ -24,11 +25,11 @@ def show_info(request, id):
         # 'description': 'Buku ini terdiri atas dua bagian. Bagian pertama mengambil sudut pandang seorang mahasiswa aktivis bernama Laut, menceritakan bagaimana Laut dan kawan-kawannya menyusun rencana, berpindah-pindah dalam pelarian, hingga tertangkap oleh pasukan rahasia. Sedangkan bagian kedua dikisahkan oleh Asmara, adik Laut. Bagian kedua mewakili perasaan keluarga korban penghilangan paksa, bagaimana pencarian mereka terhadap kerabat mereka yang tak pernah kembali.',
         # 'stock': 1550,
         # 'jumlah_terjual': 44,
-        # 'jumlah_pembelian': 1,
+        'jumlah_pembelian': 1,
 
         'book': book,
         'reviews': reviews,
-        # 'carts': carts,
+        'cart': cart,
     }
 
     return render(request, "book_info.html", context)
@@ -61,10 +62,21 @@ def delete_book(request, id):
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('book_info:show_info'))
 
-@csrf_exempt
-def increment_amount(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    member = request.user 
+def get_cart_json(request):
+    carts = Cart.objects.filter(member=request.user.account)
+    cart_list = []
+    for cart in carts:
+        cart_dict = {
+            'member': cart.member,
+            'book': cart.book,
+            'jumlah_pembelian': cart.jumlah_pembelian,
+        }
+        cart_list.append(cart_dict)
+    return JsonResponse(cart_list, safe=False)
+
+def increment_amount(request, id):
+    book = get_object_or_404(Book, pk=id)
+    member = request.user.account 
 
     cart_entry, created = Cart.objects.get_or_create(member=member, book=book)
     if cart_entry.jumlah_pembelian < book.stock:
@@ -73,10 +85,9 @@ def increment_amount(request, book_id):
 
     return JsonResponse({'success': True})
 
-@csrf_exempt
-def decrement_amount(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    member = request.user
+def decrement_amount(request, id):
+    book = get_object_or_404(Book, pk=id)
+    member = request.user.account
 
     cart_entry, created = Cart.objects.get_or_create(member=member, book=book)
     if cart_entry.jumlah_pembelian > 0:
@@ -84,3 +95,24 @@ def decrement_amount(request, book_id):
         cart_entry.save()
 
     return JsonResponse({'success': True})
+
+def get_review_json(request, id):
+    book = get_object_or_404(Book, pk=id)
+    reviews = Review.objects.filter(book=book)
+    return HttpResponse(serializers.serialize('json', reviews))
+
+def search_review_json(request, search_mode):
+    if request.method == "POST":
+        if search_mode == "semua":
+            reviews = Review.objects.all()
+        elif search_mode == "5":
+            reviews = Review.objects.filter(rating=5)
+        elif search_mode == "4":
+            reviews = Review.objects.filter(rating=4)
+        elif search_mode == "3":
+            reviews = Review.objects.filter(rating=3)
+        elif search_mode == "2":
+            reviews = Review.objects.filter(rating=2)
+        elif search_mode == "1":
+            reviews = Review.objects.filter(rating=1)
+        return HttpResponse(serializers.serialize('json', reviews))
