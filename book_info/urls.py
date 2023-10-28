efbf926e9f5dc43a612213f3ae6684d844b3a575
shfiradifa <firaradifa@gmail.com>
2023-10-28 15:13:26 +0700

    upd book info

--- book_info/urls.py
+++ book_info/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from book_info.views import show_info, increment_amount, decrement_amount
+from book_info.views import show_info, edit_book, delete_book, increment_amount, decrement_amount
 
 app_name = 'book_info'
 
 urlpatterns = [
-    path('', show_info, name='show_info'),
+    path('<int:id>/', show_info, name='show_info'),
+    path('edit-book/<int:id>', edit_book, name='edit_book'),
+    path('delete/<int:id>', delete_book, name='delete_book'),
     path('increment-amount/<int:book_id>/', increment_amount, name='increment_amount'),
     path('decrement-amount/<int:book_id>/', decrement_amount, name='decrement_amount'),
 ]
--- book_info/views.py
+++ book_info/views.py
@@ -1,28 +1,65 @@
-from django.http import JsonResponse
+from django import template
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from book.models import Book
 from account.models import Account, Review
+from book_info.forms import BookForm
 from book_info.models import Cart
+from main.models import Admin
 
 # Create your views here.
-def show_info(request, book_id=1):
-    # book = get_object_or_404(Book, id=book_id)
-    # member = request.user
-    # cart_entry, created = Cart.objects.get_or_create(member=member, book=book)
+def show_info(request, id):
+    book = get_object_or_404(Book, pk=id)
+    reviews = Review.objects.filter(book=book)
+    # carts = Cart.objects.filter(member=request.user)
     context = {
-        'authors': 'Leila S. Chudori',
-        'title': 'Laut Bercerita',
-        'categories': 'Fantasi',
-        'price': 109000,
-        'description': 'Buku ini terdiri atas dua bagian. Bagian pertama mengambil sudut pandang seorang mahasiswa aktivis bernama Laut, menceritakan bagaimana Laut dan kawan-kawannya menyusun rencana, berpindah-pindah dalam pelarian, hingga tertangkap oleh pasukan rahasia. Sedangkan bagian kedua dikisahkan oleh Asmara, adik Laut. Bagian kedua mewakili perasaan keluarga korban penghilangan paksa, bagaimana pencarian mereka terhadap kerabat mereka yang tak pernah kembali.',
-        'stock': 1550,
-        'jumlah_terjual': 44,
-        'jumlah_pembelian': 1,
-        # 'initial_quantity': cart_entry.jumlah_pembelian,
+        # dummy
+        # 'authors': 'Leila S. Chudori',
+        # 'title': 'Laut Bercerita',
+        # 'categories': 'Fantasi',
+        # 'price': 109000,
+        # 'description': 'Buku ini terdiri atas dua bagian. Bagian pertama mengambil sudut pandang seorang mahasiswa aktivis bernama Laut, menceritakan bagaimana Laut dan kawan-kawannya menyusun rencana, berpindah-pindah dalam pelarian, hingga tertangkap oleh pasukan rahasia. Sedangkan bagian kedua dikisahkan oleh Asmara, adik Laut. Bagian kedua mewakili perasaan keluarga korban penghilangan paksa, bagaimana pencarian mereka terhadap kerabat mereka yang tak pernah kembali.',
+        # 'stock': 1550,
+        # 'jumlah_terjual': 44,
+        # 'jumlah_pembelian': 1,
+
+        'book': book,
+        'reviews': reviews,
+        # 'carts': carts,
     }
 
     return render(request, "book_info.html", context)
+
+register = template.Library()
+@register.filter(name='is_admin')
+def is_admin(user):
+    return Admin.objects.filter(user=user).exists()
+
+def edit_book(request, id):
+    # Get book berdasarkan ID
+    book = Book.objects.get(pk = id)
+
+    # Set book sebagai instance dari form
+    form = BookForm(request.POST or None, instance=book)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman book-info
+        form.save()
+        return HttpResponseRedirect(reverse('book_info:show_info', args=[str(book.pk)]))
+
+    context = {'form': form}
+    return render(request, "edit_book.html", context)
+
+def delete_book(request, id):
+    # Get data berdasarkan ID
+    book = Book.objects.get(pk = id)
+    # Hapus data
+    book.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('book_info:show_info'))
 
 @csrf_exempt
 def increment_amount(request, book_id):
