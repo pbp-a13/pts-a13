a92aa228152c13968a21eb1fac7451165fc63caa
DaWanAnOnli <joel.rompis@gmail.com>
2023-10-29 13:29:07 +0700

    added switch is_admin_mode for admin

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from account.models import Account
 from django.contrib.auth.models import User
 
 class Admin(Account):
+    is_admin_mode = models.BooleanField()
     account = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     orders_completed = models.IntegerField(default=0)
     books_added = models.IntegerField(default=0)
