f7c4ebb80d7681e2b9dcee5633e2f787c9842eb2
kezialasma <kezia.lasma@ui.ac.id>
2023-10-29 15:48:03 +0700

    create button for admin (ajax belum berfungsi)

--- order/views.py
+++ order/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from order.models import Order
+from .models import Order
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def member_orderlist_view(request):
     return render(request, 'member_orderlist.html')
+
+@csrf_exempt
+def update_order(request):
+    if request.is_ajax() and request.method == "POST":
+        order_id = request.POST.get('order_id', None)
+        if order_id:
+            try:
+                order = Order.objects.get(pk=order_id)
+                order.is_completed = True
+                order.save()
+                data = {'is_completed': True}
+                return JsonResponse(data)
+            except Order.DoesNotExist:
+                return JsonResponse({"error": "Order not found"}, status=404)
+        else:
+            return JsonResponse({"error": "Invalid request"}, status=400)
+    else:
+        return JsonResponse({"error": "Invalid request"}, status=400)
+
+
